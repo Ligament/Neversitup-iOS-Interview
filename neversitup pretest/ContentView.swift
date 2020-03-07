16a1a796b71a703072b151a099e521c3401fdcd7
ligament <ntrances@hotmail.com>
2020-03-07 21:43:09 +0700

    Initial Commit

---
+++ neversitup pretest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  neversitup pretest
+//
+//  Created by admin on 7/3/2563 BE.
+//  Copyright © 2563 Ligament Studio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
